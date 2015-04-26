822aae27e403609fc6b476a400ee8725b554468f
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2015-04-25 23:02:37 -0700

    Set up landing page, navigation, and footer
    * Add logo image
    * Create hero and nav
    * Create CSS keyboard and hover effect
    * Create CSS notebook
    * Create about section on landing, add coffee image and primary cta styling
    * Update highlighting and colors
    * Create footer styles and content
    * Create recent work section
    * Create responsive landing page
    * Update README with landing image
    * Add Dribbble CTA to work section
    * Set up Divshot and jQuery
    * Add sliding animation in work section
    * Add analytics to site

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
-//= require_tree .
+// = require_tree .
+$(document).ready(function(){
+        // handle the mouseenter functionality
+        $(".landing-work-featured-image").mouseenter(function(){
+            $(this).addClass("hover");
+        })
+        // handle the mouseleave functionality
+        .mouseleave(function(){
+            $(this).removeClass("hover");
+        });
+});
